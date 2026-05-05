ed08c7b71c601f646e98fc20ab7e71612e5c0eac
dineshsaa93-boop <dineshsaa93@gmail.com>
2026-05-05 22:00:05 +0530

    Create page.js

---
+++ app/app/watch/page.js
@@ -0,0 +1,13 @@
+export default function Watch() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>🏋️ Training Video</h1>
+
+      <iframe
+        width="100%"
+        height="200"
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+      ></iframe>
+    </div>
+  );
+}
