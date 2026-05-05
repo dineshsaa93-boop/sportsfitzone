8cb6b363049f1d33c1ee14a5ea7bc0f20dfa67a6
dineshsaa93-boop <dineshsaa93@gmail.com>
2026-05-05 16:34:56 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <h1>Sports Fit Zone 🚀</h1>;
+  return (
+    <div>
+      <h1>Sports Fit Zone 🚀</h1>
+
+      <h2>Products</h2>
+
+      <ul>
+        <li>🏏 Cricket Bat - ₹999</li>
+        <li>🏸 Badminton Racket - ₹499</li>
+        <li>⚽ Football - ₹799</li>
+      </ul>
+    </div>
+  );
 }
