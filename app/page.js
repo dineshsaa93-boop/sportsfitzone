3f0cd38ef4e8b0ed40de56e4e450d701e4a3aae8
dineshsaa93-boop <dineshsaa93@gmail.com>
2026-05-05 16:41:44 +0530

    added buttons

--- app/page.js
+++ app/page.js
@@ -1,15 +1,28 @@
 export default function Home() {
   return (
-    <div>
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      
       <h1>Sports Fit Zone 🚀</h1>
 
       <h2>Products</h2>
 
       <ul>
-        <li>🏏 Cricket Bat - ₹999</li>
-        <li>🏸 Badminton Racket - ₹499</li>
-        <li>⚽ Football - ₹799</li>
+        <li>
+          🏏 Cricket Bat - ₹999 
+          <button style={{ marginLeft: "10px" }}>Buy</button>
+        </li>
+
+        <li>
+          🎾 Badminton Racket - ₹499 
+          <button style={{ marginLeft: "10px" }}>Buy</button>
+        </li>
+
+        <li>
+          ⚽ Football - ₹799 
+          <button style={{ marginLeft: "10px" }}>Buy</button>
+        </li>
       </ul>
+
     </div>
   );
 }
