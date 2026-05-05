71ecaafa25b15425445d5a05861db6a9d8aad985
dineshsaa93-boop <dineshsaa93@gmail.com>
2026-05-05 21:46:34 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
-export default function RootLayout({ children }) {
+<link rel="manifest" href="/manifest.json" />
+<meta name="theme-color" content="#0f172a" />export default function RootLayout({ children }) {
   return (
     <html>
       <body>{children}</body>
