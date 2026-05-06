a51fe396eaf06fa1b9cf850374f74be0da379f38
dineshsaa93-boop <dineshsaa93@gmail.com>
2026-05-06 06:11:43 +0530

    cources

---
+++ app/app/courses/page.js
@@ -0,0 +1,13 @@
+export default function Courses() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>📚 Sports Courses</h1>
+
+      <a href="/watch">
+        <button style={{ padding: 10, marginTop: 20 }}>
+          🏏 Cricket Training
+        </button>
+      </a>
+    </div>
+  );
+}
