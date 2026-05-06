db288efbdfc885c9e8a04492363c3a059e4b3ac5
dineshsaa93-boop <dineshsaa93@gmail.com>
2026-05-06 06:15:00 +0530

    Update page.js

--- app/app/courses/page.js
+++ app/app/courses/page.js
@@ -1,13 +1,3 @@
-export default function Courses() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>📚 Sports Courses</h1>
-
-      <a href="/watch">
-        <button style={{ padding: 10, marginTop: 20 }}>
-          🏏 Cricket Training
-        </button>
-      </a>
-    </div>
-  );
-}
+<a href="/watch">
+  <button>Start Training</button>
+</a>
