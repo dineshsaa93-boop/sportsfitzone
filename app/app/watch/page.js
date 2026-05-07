0ea552e0876a7b386abcbdb43f4d5993ad95d49e
dineshsaa93-boop <dineshsaa93@gmail.com>
2026-05-07 19:48:27 +0530

    Update page.js

--- app/app/watch/page.js
+++ app/app/watch/page.js
@@ -1,1 +1,15 @@
-
+export default function WatchPage() {
+  return (
+    <div style={{
+      background: "#000",
+      color: "white",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "30px"
+    }}>
+      WATCH PAGE
+    </div>
+  )
+}
