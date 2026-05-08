9cbae48ab47487b18d31e58e35be6ddd78bdfcf7
dineshsaa93-boop <dineshsaa93@gmail.com>
2026-05-08 06:41:13 +0530

    Dp

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,1 +1,13 @@
-
+export default function TrainingPage() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      background: "#021224",
+      color: "white",
+      padding: "30px"
+    }}>
+      <h1>Training Page</h1>
+      <p>All training videos will appear here 🔥</p>
+    </div>
+  );
+}
