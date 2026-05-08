1e9639182ee78d90e49c80583c1ed67f33b604a1
dineshsaa93-boop <dineshsaa93@gmail.com>
2026-05-08 06:06:10 +0530

    Create page.js

---
+++ app/training/page.js
@@ -0,0 +1,12 @@
+export default function TrainingPage() {
+  return (
+    <div style={{
+      background:"#020817",
+      color:"white",
+      minHeight:"100vh",
+      padding:"20px"
+    }}>
+      <h1>Training Page</h1>
+    </div>
+  )
+}
