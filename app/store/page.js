67d7f5ed13c249afc1e69c4bc8c7941c942c5b99
dineshsaa93-boop <dineshsaa93@gmail.com>
2026-05-08 14:45:12 +0530

    Update page.js

--- app/store/page.js
+++ app/store/page.js
@@ -1,13 +1,13 @@
-export default function TrainingPage() {
+export default function StorePage() {
   return (
     <div style={{
       minHeight: "100vh",
-      background: "#021224",
+      background: "#020817",
       color: "white",
       padding: "30px"
     }}>
-      <h1>Training Page</h1>
-      <p>All training videos will appear here 🔥</p>
+      <h1>Sports Store</h1>
+      <p>Sports items and equipment.</p>
     </div>
-  );
+  )
 }
