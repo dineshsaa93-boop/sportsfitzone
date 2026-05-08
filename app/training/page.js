eefbc724408ea1557dcf45e981c36253140221f1
dineshsaa93-boop <dineshsaa93@gmail.com>
2026-05-08 14:43:50 +0530

    Update page.js

--- app/training/page.js
+++ app/training/page.js
@@ -2,12 +2,12 @@ export default function TrainingPage() {
   return (
     <div style={{
       minHeight: "100vh",
-      background: "#021224",
+      background: "#020817",
       color: "white",
       padding: "30px"
     }}>
       <h1>Training Page</h1>
-      <p>All training videos will appear here 🔥</p>
+      <p>Workout videos and sports training will appear here.</p>
     </div>
-  );
+  )
 }
