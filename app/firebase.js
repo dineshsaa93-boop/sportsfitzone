1cb6480e13b2a76f280b0a61e631073e2fb23d7c
dineshsaa93-boop <dineshsaa93@gmail.com>
2026-05-08 15:22:52 +0530

    Update firebase.js

--- app/firebase.js
+++ app/firebase.js
@@ -1,1 +1,15 @@
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDultie469Qm2tiMmJIKxnL1UaBs4CmzBQ",
+  authDomain: "sports-fit-zone.firebaseapp.com",
+  projectId: "sports-fit-zone",
+  storageBucket: "sports-fit-zone.firebasestorage.app",
+  messagingSenderId: "679875456871",
+  appId: "1:679875456871:web:1507ddc73009cc53ec89c2",
+  measurementId: "G-5QE98D2M4D"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
