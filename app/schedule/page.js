71ba518f9bd3d3032b06651020b372f3bd391598
dineshsaa93-boop <dineshsaa93@gmail.com>
2026-05-11 21:00:24 +0530

    Update page.js

--- app/schedule/page.js
+++ app/schedule/page.js
@@ -1,1 +1,74 @@
+export default function SchedulePage() {
 
+  const plans = [
+    {
+      day: "Monday",
+      workout: "Chest Workout 💪"
+    },
+
+    {
+      day: "Tuesday",
+      workout: "Cardio + Running 🏃"
+    },
+
+    {
+      day: "Wednesday",
+      workout: "Leg Workout 🦵"
+    },
+
+    {
+      day: "Thursday",
+      workout: "Football Practice ⚽"
+    },
+
+    {
+      day: "Friday",
+      workout: "Shoulder Workout 🔥"
+    }
+  ];
+
+  return (
+    <div
+      style={{
+        background: "#020617",
+        minHeight: "100vh",
+        color: "white",
+        padding: "20px",
+        fontFamily: "sans-serif"
+      }}
+    >
+      <h1
+        style={{
+          color: "#39ff14",
+          marginBottom: "25px"
+        }}
+      >
+        📅 Workout Schedule
+      </h1>
+
+      {plans.map((plan, index) => (
+
+        <div
+          key={index}
+          style={{
+            background: "#111827",
+            padding: "18px",
+            borderRadius: "18px",
+            marginBottom: "16px"
+          }}
+        >
+          <h2>{plan.day}</h2>
+
+          <p
+            style={{
+              color: "#94a3b8",
+              marginTop: "8px"
+            }}
+          >
+            {plan.workout}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
