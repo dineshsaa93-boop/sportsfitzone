41f193a47edad7127963595cf4557b93856b2630
dineshsaa93-boop <dineshsaa93@gmail.com>
2026-06-14 17:55:08 +0530

    Update page.js

--- app/coach/live/page.js
+++ app/coach/live/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { auth, db } from "../../../firebase";
+import { auth, db } from "../../firebase";
 import { collection, addDoc } from "firebase/firestore";
 import { Video, Calendar } from "lucide-react";
 
