97915e27a2fc4302485c4d321c5b37f3e76599f2
dineshsaa93-boop <dineshsaa93@gmail.com>
2026-06-14 17:54:28 +0530

    Update page.js

--- app/coach/courses/page.js
+++ app/coach/courses/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { auth, db } from "../../../firebase";
+import { auth, db } from "../../firebase";
 import { collection, addDoc } from "firebase/firestore";
 import { BookOpen, Upload } from "lucide-react";
 
